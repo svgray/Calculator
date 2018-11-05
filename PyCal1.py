d34ab6ca0998ee5fa80ba05b40e08c9b8e197eac
svgray <44779068+svgray@users.noreply.github.com>
2018-11-05 12:34:34 -0500

    PyCal1.py
    My first bits of code for the calculator program. Not complete. Still need to add other operators within the system, add comments and documentation.

---
+++ PyCal1.py
@@ -0,0 +1,33 @@
+# Calculator
+Creating A Simple Calculator
+while True:
+   print("Options:")
+   print("Enter 'add' to add two numbers")
+   print("Enter 'subtract' to subtract two numbers")
+   print("Enter 'multiply' to multiply two numbers")
+   print("Enter 'divide' to divide two numbers")
+   print("Enter 'quit' to end the program")
+   user_input = input(": ")
+
+   if user_input == "quit":
+      break
+   elif user_input == "add":
+      ...
+   elif user_input == "subtract":
+      ...
+   elif user_input == "multiply":
+      ...
+   elif user_input == "divide":
+      ...
+   else:
+      print("Unknown input")
+      
+  elif user_input == "add":
+  num1 = float(input("Enter a number: "))
+  num2 = float(input("Enter another number: "))    
+  elif user_input == "add":
+  num1 = float(input("Enter a number: "))
+  num2 = float(input("Enter another number: "))
+  result = str(num1 + num2)
+  print("The answer is " + result)
+    
